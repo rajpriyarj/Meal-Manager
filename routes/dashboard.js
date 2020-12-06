4ad4e843bed37ea6fe9665016096fd1985d4e9fd
Rajpriya <rajpriyarj75@gmail.com>
2020-12-06 11:46:29 +0530

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 const Dashboard = require('../controllers/c_dashboard')
+const {authenticate} = require('../controllers/auth')
 
-router.get('/dashboard', Dashboard.getdashboard);
+router.get('/dashboard', authenticate, Dashboard.getdashboard);
 
 module.exports = router
