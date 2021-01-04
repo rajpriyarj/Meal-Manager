50c6c0b9b78916a653b5c1f37b97ad27218d57b4
Rajpriya <rajpriyarj75@gmail.com>
2021-01-04 19:23:53 +0530

    updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,72 +1,39 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './App.scss';
+import 'font-awesome/css/font-awesome.min.css';
+import Login from "./Components/studentPortal/SignIn";
+// import Login from "./Components/adminPortal/Login"
 
-import SignUp from './Components/studentPortal/SignUp';
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { apiResponse: "" };
-// }
-
-// callAPI() {
-//     fetch("http://localhost:7000/dashboard")
-//         .then(res => res.text())
-//         .then(res => this.setState({ apiResponse: res }))
-//         .catch(err => err);
-// }
-
-// componentWillMount() {
-//     this.callAPI();
-// }
-
-// render(){
-//   return(
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo"/>
-//         <h1 className="App-title">Welcome</h1>
-//       </header>
-//   <p className="App-intro">{this.state.apiResponse}</p>
-//     </div>
-//   )
-// }
-// }
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand">React Axios Tutorial</a>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to={"/signup"}>Create User</Link>
-              </li>
-              {/* <li className="nav-item">
-                <Link className="nav-link" to={"/users"}>Users List</Link>
-              </li> */}
-            </ul>
-          </div>
-        </nav>
-      </header>
-
+  return (
       <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <Switch>
-              <Route exact path='/' component={SignUp} />
-              <Route path="/signup" component={SignUp} />
-            </Switch>
-          </div>
+        <div className="header">
+          Meal Manager
+        </div>
+
+        <div className="card">
+          <h2>Admin Login</h2>
+          <i className="fa fa-arrow-right">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
+          </i>
+          <div className="pic"></div>
+          <button>
+          </button>
+        </div>
+          <div className="card card2">
+          <h2>Student Login</h2>
+          <i className="fa fa-arrow-right"></i>
+          <div className="pic"></div>
+          <button>
+          </button>
         </div>
       </div>
-    </div>
-  </Router>
   );
 }
 
--- front-end/src/Components/studentPortal/SignIn.js
+++ front-end/src/Components/studentPortal/SignIn.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
 
-class SignIn extends Component {
+class Login extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.onChangeLibraryId = this.onChangeLibraryId.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            libraryId: '',
+            password: ''
+        }
+    }
+
+    onChangeLibraryId(e) {
+        this.setState({ libraryId: e.target.value })
+    }
+
+    onChangePassword(e) {
+        this.setState({ password: e.target.value })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+    const studentObject = {
+        libraryId: this.state.libraryId,
+        password: this.state.password,
+    };
+
+    axios.post('http://localhost:7000/student/login', studentObject)
+        .then((res) => {
+            console.log(res.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+
+    this.setState({ 
+        libraryId: '',
+        password: '',
+    })
+}
+
+
     render() {
         return (
             <div className="wrapper">
-                <form>
+                <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Library Id</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" value={this.state.libraryId} onChange={this.onChangeLibraryId} className="form-control" />
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" value={this.state.password} onChange={this.onChangePassword} className="form-control" />
                     </div>
                     <div className="form-group">
                         <input type="submit" value="Create User" className="btn btn-success btn-block" />
@@ -22,4 +66,4 @@ class SignIn extends Component {
     }
 }
 
-export default SignIn;
+export default Login;
